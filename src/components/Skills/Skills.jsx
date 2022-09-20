ed453e879a4c0db27c50b3cff8a0828b27905439
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-20 11:21:24 +0100

    add font-awesome icons

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import styles from './Skills.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faGithub, faReact, faHtml5, faSass, faCss3, faJsSquare} from '@fortawesome/free-brands-svg-icons';
+
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+    <h2>Technical Skills</h2>
+
+
+  
+    <FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon>
+    <FontAwesomeIcon icon={faReact}></FontAwesomeIcon>
+    <FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon>
+    <FontAwesomeIcon icon={faCss3}></FontAwesomeIcon>
+    <FontAwesomeIcon icon={faSass}></FontAwesomeIcon>
+    <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+
+
+
+    <p>BEM </p>
+    </>
   )
 }
 
