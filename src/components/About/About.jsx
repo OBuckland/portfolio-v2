1f1d4f9fc60a3e2dae790a24a05cf755379b3205
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-21 08:31:51 +0100

    set up about section boilerplate html

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,11 @@ import styles from './About.module.scss'
 
 const About = () => {
   return (
+    <section id="aboutSection" className={styles.aboutContainer}>
     <div>About</div>
+    <h1> About Me </h1>
+    <p> Description </p>
+    </section>
   )
 }
 
