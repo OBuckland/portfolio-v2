05d35caf8c380c063bf5fe23043d2cef5c0e4d6f
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-21 08:35:58 +0100

    add working links to nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './NavBar.module.scss';
+import About from '../About/About';
 
 const NavBar = () => {
   return (
     <nav className={styles.navContainer}>
     <div className={styles.nameTitle}>Olivia Buckland</div>
     <div className={styles.navLinksContainer}>
-      <a href="">Home</a>
-      <a href="">About</a>
-      <a href="">Projects</a>
+      <a href="#homeSection">Home</a>
+      <a href="#aboutSection">About</a>
+      <a href="#projectsSection">Projects</a>
       <button className={styles.contactBtn}>Get in Touch</button>
     </div>
     </nav>
