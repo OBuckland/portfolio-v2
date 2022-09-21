9fbb5b1a209e1533a0ba74b34f9affc4c6511f3c
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-21 08:31:18 +0100

    add logic to create cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import styles from './Projects.module.scss';
+import Card from './../Card';
+import { projectData } from '../../projectData';
 
 const Projects = () => {
+// const {project} = props;
+  // const renderCards= () => {
+  //   return <Card project={project}/>
+  // }
+
   return (
-    <div>Projects</div>
+    <section className={styles.cardsContainer}>
+{projectData.map(project => <Card project={project} key={project.name}/>)}
+
+    </ section>
   )
 }
 
