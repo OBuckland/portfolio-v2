c268e3a0381b15a89ba86d58ca928acf76de9049
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-22 14:30:26 +0100

    add skeleton for about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,9 +4,35 @@ import styles from './About.module.scss'
 const About = () => {
   return (
     <section id="aboutSection" className={styles.aboutContainer}>
-    <div>About</div>
-    <h1> About Me </h1>
-    <p> Description </p>
+      <h2>About me</h2>
+      <div>
+
+        <p className={styles.aboutIntro}>Intro about me :) Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo nam eum modi architecto illo repellendus dicta, deleniti doloremque ullam dolorum, magnam dignissimos qui sequi est. Quia soluta esse dolorem quae.</p>
+        <h3>Experience</h3>
+
+        <div className={styles.textContainer}>
+          <h4>
+            Job title
+          </h4>
+          <p>01/10/2021 - 01/09/2022</p>
+          <ul>
+            <li>About the job</li>
+            <li>Lorem ipsum dolor sit.</li>
+          </ul>
+        </div>
+
+        <div  className={styles.textContainer}>
+          <h4>
+            Job title
+          </h4>
+          <p>Dates of job</p>
+          <ul>
+            <li>About the job</li>
+            <li>Lorem ipsum dolor sit.</li>
+          </ul>
+        </div>
+
+      </div>
     </section>
   )
 }
