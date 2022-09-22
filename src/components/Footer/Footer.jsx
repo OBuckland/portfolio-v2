4ef154a3af7d94f860f83ad24eff2738aa4f91c0
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-22 14:31:36 +0100

    add icons to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
-    <p> Email </p>
+    <p className={styles.contactText}>  Get in touch </p>
     <div className={styles.footerLinks}>
-    <p> GitHub </p>
-    <p> LinkedIn </p>
-    <p> CV </p>
+    <FontAwesomeIcon className={styles.icons} icon={faEnvelope}/>
+    <FontAwesomeIcon className={styles.icons} icon={faGithub}/>
+    <FontAwesomeIcon className={styles.icons} icon={faLinkedin}/>
+    <FontAwesomeIcon className={styles.icons} icon={faFile}/>
     </div>
     </footer>
   )
