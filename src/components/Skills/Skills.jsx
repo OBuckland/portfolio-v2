cedb5e979d9e2407dcc8a9ea79cd8f2a32a14843
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-22 14:28:09 +0100

    add stylings for skill section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,27 +1,68 @@
 import React from 'react';
 import styles from './Skills.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faGithub, faReact, faHtml5, faSass, faCss3, faJsSquare} from '@fortawesome/free-brands-svg-icons';
+import {faGithub, faReact, faHtml5, faSass, faCss3, faJsSquare, faNode, faFigma} from '@fortawesome/free-brands-svg-icons';
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
+import { faB, faVials, faFire } from '@fortawesome/free-solid-svg-icons';
 
 
 const Skills = () => {
   return (
-    <>
+    <section className={styles.skillsContainer}>
     <h2>Technical Skills</h2>
+    <p>Below are some of the tools I can use/ I am familiar with the following technologies/ tech stack I have been working with..</p>
 
-
-  
-    <FontAwesomeIcon icon={faJsSquare}/>
-    <FontAwesomeIcon icon={faReact}/>
-    <FontAwesomeIcon icon={faHtml5}/>
-    <FontAwesomeIcon icon={faCss3}/>
-    <FontAwesomeIcon icon={faSass}/>
-    <FontAwesomeIcon icon={faGithub}/>
-
-
-
-    <p>BEM </p>
-    </>
+    <ul className={styles.skillsListContainer}>
+      <li className={styles.skills}>
+      <FontAwesomeIcon icon={faReact}/>
+        React
+      </li>
+      <li className={styles.skills}>
+        <FontAwesomeIcon icon={faJsSquare}/>
+        JavaScript
+      </li>
+      <li className={styles.skills}>
+        <FontAwesomeIcon icon={faHtml5}/>
+        HTML5
+      </li>
+      <li className={styles.skills}>
+        <FontAwesomeIcon icon={faCss3}/>
+        CSS3
+      </li>
+      <li className={styles.skills}>
+        <FontAwesomeIcon icon={faSass}/>
+        Sass
+      </li>
+      <li className={styles.skills}>
+        <FontAwesomeIcon icon={faCodeBranch} />
+        Git
+      </li>
+      <li className={styles.skills}>
+        <FontAwesomeIcon className={styles.icons} icon={faGithub}/>
+        GitHub
+      </li>
+      <li className={styles.skills}>
+       <FontAwesomeIcon className={styles.icons} icon={faB}/>
+        BEM
+      </li>
+      <li className={styles.skills}>
+       <FontAwesomeIcon className={styles.icons} icon={faNode}/>
+        node.js
+      </li>
+      <li className={styles.skills}>
+       <FontAwesomeIcon className={styles.icons} icon={faFire}/>
+        Firebase
+      </li>
+      <li className={styles.skills}>
+       <FontAwesomeIcon className={styles.icons} icon={faFigma}/>
+        Figma
+      </li>
+      <li className={styles.skills}>
+       <FontAwesomeIcon className={styles.icons} icon={faVials}/>
+         Jest / Enzyme / React Testing Library
+      </li>
+    </ul>
+    </ section>
   )
 }
 
