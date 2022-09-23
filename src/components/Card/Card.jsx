e51306ea758a23e68ca83658bdd1626bbca8801f
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-23 12:21:59 +0100

    move html to cardFront and create flip logic

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,34 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react'
 import styles from './Card.module.scss';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode, faEye} from '@fortawesome/free-solid-svg-icons';
+import CardBack from '../CardBack'
+import CardFront from '../CardFront'
 
 
 const Card = (props) => {
+  const [isFaceDown, setIsFaceDown] = useState(false);
+  const flipStyles = isFaceDown ? styles.faceDown : "";
 
-  const {name, description, image, technology, gitHub, url} = props.project;
+
+  const {project} = props;
+  // const {name, description, image, technology, gitHub, url} = props.project;
 
   return (
+    <section 
 
-      <div className={styles.card}>
-
-        <div className={styles.pictureContainer}>
-          <img className={styles.cardImage} src={image} alt="" />
-        </div>
-
-        <div className={styles.textSection}>
-          <h3 className={styles.technology}>{technology}</h3>
-          <h1>{name}</h1>
-          <p>{description}</p>
-        </div>
-
-        <div className={styles.links}>
-        <a href={gitHub}>Code <FontAwesomeIcon icon={faCode} /></a>
-        <a href={url}>  Preview <FontAwesomeIcon icon={faEye} /> </a>
-        </div>
-
+    className={`${flipStyles} ${styles.card}`}
+    onClick={() => setIsFaceDown(!isFaceDown)}>
+      
+      <div className={styles.front}>
+        <CardFront project={project} />
       </div>
+
+      <div className={styles.back}>
+        <CardBack project={project}/>
+      </div>
+
+
+    </section>
   )
 }
 
