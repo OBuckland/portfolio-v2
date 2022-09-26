6b387350d7c844a11f97f1e7b019cafb5f39be28
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-26 11:14:29 +0100

    reformat and add text to about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import styles from './About.module.scss'
+import styles from './About.module.scss';
+import nology from '../../assets/nology_logo2.jpeg';
+
 
 const About = () => {
   return (
@@ -7,12 +9,14 @@ const About = () => {
       <h2>About me</h2>
       <div>
 
-        <p className={styles.aboutIntro}>Intro about me :) Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo nam eum modi architecto illo repellendus dicta, deleniti doloremque ullam dolorum, magnam dignissimos qui sequi est. Quia soluta esse dolorem quae.</p>
+        <p className={styles.aboutIntro}>Nurse turned software developer.  Since completing my Children's Nursing Degree, I have worked in a variety of roles. In 2021 I decided to pursue a career change and enrolled on a self-paced software development course. Now I am looking for my first role as a front-end developer, to build on the skills and knowledge that I have gained. </p>
         <h3>Experience</h3>
+
+        <div className={styles.jobContainer}>
 
         <div className={styles.textContainer}>
           <h4>
-            Job title
+            Trainee Software Developer
           </h4>
           <p>01/10/2021 - 01/09/2022</p>
           <ul>
@@ -22,14 +26,28 @@ const About = () => {
         </div>
 
         <div  className={styles.textContainer}>
+          {/* <img src={nology} alt="" /> */}
           <h4>
-            Job title
+            Immunisation Nurse
           </h4>
           <p>Dates of job</p>
           <ul>
             <li>About the job</li>
             <li>Lorem ipsum dolor sit.</li>
           </ul>
+        </div>
+
+        <div  className={styles.textContainer}>
+          <h4>
+            Parlimentary Questions Team
+          </h4>
+          <p>Dates of job</p>
+          <ul>
+            <li>About the job</li>
+            <li>Lorem ipsum dolor sit.</li>
+          </ul>
+        </div>
+
         </div>
 
       </div>
