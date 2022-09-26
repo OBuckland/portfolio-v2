6a95c97ef5c5f9ba3f719d0b50e551efb1cdbab7
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-09-26 09:11:10 +0100

    Add scroll animation

--- src/components/HeroPage/HeroPage.jsx
+++ src/components/HeroPage/HeroPage.jsx
@@ -3,6 +3,8 @@ import styles from './HeroPage.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown, faCat} from '@fortawesome/free-solid-svg-icons';
 import profileImage from '../../assets/portfolio-profile.jpg';
+import { Link, animateScroll as scroll } from "react-scroll";
+
 
 const HeroPage = () => {
   return (
@@ -11,10 +13,18 @@ const HeroPage = () => {
         <div className={styles.heroTextContainer}>
         <h3 className={styles.introText}>{  <FontAwesomeIcon icon={faCat}></FontAwesomeIcon>}  Hey! I'm Olivia Buckland</h3>
         <h1 className={styles.headingText}> A Junior Software Developer based in Sydney.</h1>
-        <button  className={styles.heroPageBtn}  href="#projectsSection" >Take a look at my work {  <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon>} </button>
+        <Link className={styles.heroPageBtn} 
+    activeClass="active"
+    to="projectsSection"
+    spy={true}
+    smooth={true}
+      >Take a look at my work {  <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon>} </Link>
+
         </div>
         <img className={styles.profileImage} src={profileImage} alt="profile" />
       </div>
+
+
     </ section>
   )
 }
