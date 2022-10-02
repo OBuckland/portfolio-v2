83cef02edd8ea42bbc49476a1b59a27efd3da2c2
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-10-02 12:27:35 +0100

    fix get in touch btn in NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NavBar.module.scss';
+import { Link, animateScroll as scroll } from "react-scroll";
+// import PropType from 'prop-types';
 import About from '../About/About';
 
 const NavBar = () => {
+
+  const [contactMenuIsOpen, setContactMenuIsOpen] = useState(false);
+
+
+  const toggleContactMenu = () => {
+    setContactMenuIsOpen(!contactMenuIsOpen)
+  };
+
+  const contactStyle = contactMenuIsOpen ? styles.menuIsOpenStyle : styles.menuIsClosedStyle;
+
+  // const [value, setValue] = useState("email");
+  // const handleChange = (event) => {
+  //   setValue(event.target.value)
+  // }
   return (
     <nav className={styles.navContainer}>
     <div className={styles.nameTitle}>Olivia Buckland</div>
+    <div className={styles.nameTitleInitals}>OB</div>
     <div className={styles.navLinksContainer}>
-      <a href="#projectsSection">Projects</a>
-      <a href="#aboutSection">About</a>
-      <button className={styles.contactBtn}>Get in Touch</button>
+      <Link 
+        activeClass="active"
+        to="projectsSection"
+        smooth={true}>
+          Projects
+      </Link>
+
+      <Link
+            activeClass="active"
+            to="aboutSection"
+            smooth={true}>
+        About
+      </Link>
+      <button onClick={toggleContactMenu} className={styles.contactBtn}>Get in Touch</button>
+
+
     </div>
+
+
+     {/* <div>
+      <select onChange={handleChange} >
+        <option>Email</option>
+        <option value="linkedIn">LinkedIn</option>
+        <option value="cv">CV</option>
+      </select>
+    </div>  */}
     </nav>
   )
 }
