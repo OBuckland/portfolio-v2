db426e602db69cc4221132cf540ceda66233749b
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-12-08 10:32:03 +1100

    add pdf link to cv

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import styles from './Footer.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import cv from "../../assets/OliviaBucklandCV.pdf";
+import styles from "./Footer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faFile } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
-    <div className={styles.footerLinks}>
+      <div className={styles.footerLinks}>
+        <a href="mailto:oliviabuckland@hotmail.com" target="_blank">
+          <FontAwesomeIcon className={styles.icons} icon={faEnvelope} />
+        </a>
 
-    <a href="mailto:oliviabuckland@hotmail.com" target="_blank"><FontAwesomeIcon className={styles.icons} icon={faEnvelope}/></a>
+        <a href="https://github.com/OBuckland" target="_blank">
+          {" "}
+          <FontAwesomeIcon className={styles.icons} icon={faGithub} />
+        </a>
 
-    <a href="https://github.com/OBuckland" target="_blank">       <FontAwesomeIcon className={styles.icons} icon={faGithub}/></a>
+        <a
+          href="https://linkedin.com/in/olivia-buckland-43b0a4167"
+          target="_blank"
+        >
+          <FontAwesomeIcon className={styles.icons} icon={faLinkedin} />
+        </a>
 
-    <a href="https://linkedin.com/in/olivia-buckland-43b0a4167" target="_blank">
-      <FontAwesomeIcon className={styles.icons} icon={faLinkedin}/></a>
-
-    <a href=""target="_blank">   <FontAwesomeIcon className={styles.icons} icon={faFile}/></a>
-    </div>
+        <a href={cv} target="_blank">
+          {" "}
+          <FontAwesomeIcon className={styles.icons} icon={faFile} />
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import styles from './NavBar.module.scss';
+import React, { useState } from "react";
+import styles from "./NavBar.module.scss";
 import { Link, animateScroll as scroll } from "react-scroll";
-// import PropType from 'prop-types';
-import About from '../About/About';
+import cv from "../../assets/OliviaBucklandCV.pdf";
+import About from "../About/About";
 
 const NavBar = () => {
-
   const [contactMenuIsOpen, setContactMenuIsOpen] = useState(false);
 
-
   const toggleContactMenu = () => {
-    setContactMenuIsOpen(!contactMenuIsOpen)
+    setContactMenuIsOpen(!contactMenuIsOpen);
   };
 
-  const contactStyle = contactMenuIsOpen ? styles.menuIsOpenStyle : styles.menuIsClosedStyle;
+  const contactStyle = contactMenuIsOpen
+    ? styles.menuIsOpenStyle
+    : styles.menuIsClosedStyle;
 
   // const [value, setValue] = useState("email");
   // const handleChange = (event) => {
@@ -21,35 +21,43 @@ const NavBar = () => {
   // }
   return (
     <nav className={styles.navContainer}>
-        <div className={styles.nameTitle}>Olivia Buckland</div>
-        <div className={styles.nameTitleInitals}>OB</div>
-        <div className={styles.navLinksContainer}>
-          <div className={styles.navLinks}>
-              <Link 
-                activeClass="active"
-                to="projectsSection"
-                smooth={true}>
-                  Projects
-              </Link>
-
-              <Link
-                    activeClass="active"
-                    to="aboutSection"
-                    smooth={true}>
-                About
+      <div className={styles.nameTitle}>Olivia Buckland</div>
+      <div className={styles.nameTitleInitals}>OB</div>
+      <div className={styles.navLinksContainer}>
+        <div className={styles.navLinks}>
+          <Link activeClass="active" to="projectsSection" smooth={true}>
+            Projects
           </Link>
-          </div>
-          <button onClick={toggleContactMenu} className={styles.contactBtn}>Get in Touch</button>
-          </div>
-    <div className={contactStyle}>
-        <a className={styles.contactLinks} href="mailto:oliviabuckland@hotmail.com" target="_blank">Email</a>
-        <a className={styles.contactLinks} href="https://linkedin.com/in/olivia-buckland-43b0a4167" target="_blank">
-          Linkedin</a> 
-        <a className={styles.contactLinks} href=""target="_blank">CV</a>
 
-      </div> 
+          <Link activeClass="active" to="aboutSection" smooth={true}>
+            About
+          </Link>
+        </div>
+        <button onClick={toggleContactMenu} className={styles.contactBtn}>
+          Get in Touch
+        </button>
+      </div>
+      <div className={contactStyle}>
+        <a
+          className={styles.contactLinks}
+          href="mailto:oliviabuckland@hotmail.com"
+          target="_blank"
+        >
+          Email
+        </a>
+        <a
+          className={styles.contactLinks}
+          href="https://linkedin.com/in/olivia-buckland-43b0a4167"
+          target="_blank"
+        >
+          Linkedin
+        </a>
+        <a className={styles.contactLinks} href={cv} target="_blank">
+          CV
+        </a>
+      </div>
 
-     {/* <div>
+      {/* <div>
       <select onChange={handleChange} >
         <option>Email</option>
         <option value="linkedIn">LinkedIn</option>
@@ -57,7 +65,7 @@ const NavBar = () => {
       </select>
     </div>  */}
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar;
