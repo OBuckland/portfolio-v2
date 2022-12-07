22dfe5a34400b3b2a073af7bef30cae781f2f0a5
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2022-12-08 10:31:45 +1100

    add padding at mobile view to project titles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,41 +1,44 @@
-import React, {useState} from 'react'
-import styles from './Card.module.scss';
-import CardBack from '../CardBack'
-import CardFront from '../CardFront'
-
+import React, { useState } from "react";
+import styles from "./Card.module.scss";
+import CardBack from "../CardBack";
+import CardFront from "../CardFront";
 
 const Card = (props) => {
   const [isFaceDown, setIsFaceDown] = useState(false);
   const flipStyles = isFaceDown ? styles.faceDown : "";
 
-
-  const {project} = props;
+  const { project } = props;
   // const {name, description, image, technology, gitHub, url} = props.project;
 
   return (
     <section className={styles.cardContainer}>
-    <div className={`${flipStyles} ${styles.card}`}
-        onClick={() => setIsFaceDown(!isFaceDown)}>      
-          <div className={styles.front}>
-            <CardFront project={project} />
-          </div>
+      <div
+        className={`${flipStyles} ${styles.card}`}
+        onClick={() => setIsFaceDown(!isFaceDown)}
+      >
+        <div className={styles.front}>
+          <CardFront project={project} />
+        </div>
 
-          <div className={styles.back}>
-            <CardBack project={project}/>
-          </div>
+        <div className={styles.back}>
+          <CardBack project={project} />
+        </div>
       </div>
-      
-    <div className={styles.titleAndTechContainer}>
-        <h3>{project.name}</h3>
+
+      <div className={styles.titleAndTechContainer}>
+        <h3 className={styles.projectTitle}>{project.name}</h3>
         <ul className={styles.technologyList}>
           {project.technologies.map((technology) => {
-            return <li className={styles.technology} key={technology}>{technology}</li>
+            return (
+              <li className={styles.technology} key={technology}>
+                {technology}
+              </li>
+            );
           })}
         </ul>
-    </div>
-
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
